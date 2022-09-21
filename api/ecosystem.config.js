4f13970b695b081dae580aeeeb11171f5f04e9cf
Ivan Kraev <ivankraev2@abv.bg>
2022-09-21 11:20:56 +0300

    fix(config file): we changed a variable name

--- api/ecosystem.config.js
+++ api/ecosystem.config.js
@@ -2,14 +2,9 @@ module.exports = {
     apps: [
         {
             name: 'msb-hub',
-            script: 'build/app.js',
-            args: '-r ts-node/register/transpile-only -r tsconfig-paths/register',
-            env_production: {
-                NODE_ENV: 'production',
-            },
-            env_development: {
-                NODE_ENV: 'development',
-            },
+            script: './build/app.js',
+            /* node_args: '-r ts-node/register/transpile-only -r tsconfig-paths/register', */
+            instances: -1
         },
     ],
 };
