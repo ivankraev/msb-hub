e35e8409f97cd3104451d64b912d23122683b5f4
Ivan Kraev <ivankraev2@abv.bg>
2022-09-23 02:51:28 +0300

    test

--- jenkins/scripts/build-images.sh
+++ jenkins/scripts/build-images.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 cd api && docker build -t msb-hub-api -f Dockerfile.prod .
-cd client && docker build -t msb-hub-client -f Dockerfile.prod .
+cd ../client && docker build -t msb-hub-client -f Dockerfile.prod .
