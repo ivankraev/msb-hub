c41b40716f239625faa600e56def0b5409c6e006
Ivan Kraev <ivankraev2@abv.bg>
2022-09-22 14:28:38 +0300

    fix

--- api/ecosystem.config.js
+++ api/ecosystem.config.js
@@ -1,10 +1,10 @@
 module.exports = {
-    apps: [
-        {
-            name: 'msb-hub',
-            script: './build/app.js',
-            /* node_args: '-r ts-node/register/transpile-only -r tsconfig-paths/register', */
-            instances: -1
-        },
-    ],
+  apps: [
+    {
+      name: 'msb-hub',
+      script: './build/app.js',
+      /* node_args: '-r ts-node/register/transpile-only -r tsconfig-paths/register', */
+      instances: -1,
+    },
+  ],
 };
