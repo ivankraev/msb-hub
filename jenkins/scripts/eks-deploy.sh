10c70ffc81c026279ed3bd7899b4087a75951c5c
Ivan Kraev <ivankraev2@abv.bg>
2022-09-23 03:15:39 +0300

    test

--- jenkins/scripts/eks-deploy.sh
+++ jenkins/scripts/eks-deploy.sh
@@ -2,7 +2,7 @@
 curl -o kubectl https://s3.us-west-2.amazonaws.com/amazon-eks/1.22.6/2022-03-09/bin/linux/amd64/kubectl
 chmod u+x ./kubectl
 ./kubectl apply -f infra/k8s/api-config.yaml
-./kubectl rollout restart deployment api-config
+./kubectl rollout restart deployment api-depl
 ./kubectl apply -f infra/k8s/client-config.yaml
-./kubectl rollout restart deployment client-config
+./kubectl rollout restart deployment client-depl
 ./kubectl apply -f infra/k8s/ingress-srv.yaml
