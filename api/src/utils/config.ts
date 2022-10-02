b288e69864e42d5b85c0b70e870801b2c12db308
Ivan Kraev <ivankraev2@abv.bg>
2022-10-03 01:27:39 +0300

    feat(n): n
    n

--- api/src/utils/config.ts
+++ api/src/utils/config.ts
@@ -9,7 +9,7 @@ const schema = Type.Object({
     default: 3000,
   }),
   DATABASE_URL: Type.String({
-    default: 'fakeUrl',
+    default: 'fakeUrlfdd',
   }),
   API_HOST: Type.String({
     default: 'http://localhost:80',
