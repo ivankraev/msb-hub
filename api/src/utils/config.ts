fdcdbd9d3b0637d259b886deab4beb7632a9d03d
Ivan Kraev <ivankraev2@abv.bg>
2022-10-03 01:44:07 +0300

    feat(sadasd): asdasd
    asdas

--- api/src/utils/config.ts
+++ api/src/utils/config.ts
@@ -9,7 +9,7 @@ const schema = Type.Object({
     default: 3000,
   }),
   DATABASE_URL: Type.String({
-    default: 'fakeUrdsss',
+    default: 'fakeUrss',
   }),
   API_HOST: Type.String({
     default: 'http://localhost:80',
