9bf880f201f35e2ea09454e86f8885f667cfc1fb
Ivan Kraev <ivankraev2@abv.bg>
2022-10-03 01:36:41 +0300

    docs(sad): asdfasf
    sda
    
    BREAKING CHANGE: asdas

--- api/src/utils/config.ts
+++ api/src/utils/config.ts
@@ -9,7 +9,7 @@ const schema = Type.Object({
     default: 3000,
   }),
   DATABASE_URL: Type.String({
-    default: 'fakeUrd',
+    default: 'fakeUrds',
   }),
   API_HOST: Type.String({
     default: 'http://localhost:80',
