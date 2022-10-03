a9f8e85ed5157a254c5ff8104de84e029859a29a
Ivan Kraev <ivankraev2@abv.bg>
2022-10-03 04:01:44 +0300

    fix(asdas): asd
    asdas

--- api/src/utils/config.ts
+++ api/src/utils/config.ts
@@ -9,7 +9,7 @@ const schema = Type.Object({
     default: 3000,
   }),
   DATABASE_URL: Type.String({
-    default: 'fassssslsss',
+    default: 'fasssslsss',
   }),
   API_HOST: Type.String({
     default: 'http://localhost:80',
